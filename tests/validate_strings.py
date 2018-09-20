338596bee2f7066c087925617e45561221f4a39e
Marco Barrera <mbarrera@thousandeyes.com>
2018-09-19 17:11:55 -0700

    MOdified validate strings to remove the counter

--- tests/validate_strings.py
+++ tests/validate_strings.py
@@ -1,8 +1,8 @@
 #/home/mbarrera/github.com/NetworkAutomation/bin/python3
 #coding=utf-8
 import sys
-word1 = "oso"
-word2 = "hola"
+word1 = "0s333sc"
+word2 = "s00s"
 print("First String:", word1)
 print ("Second String:", word2)
 #access a String word[0]
@@ -21,11 +21,9 @@ def equal_strings(word1,word2):
 def same_letters(word1,word2):
     string_size = len(word1)
     letter={}
-    count=1
     for i in range(string_size):
         if word1[i] in letter:
-            count+=1
-            letter[word1[i]]=count
+            letter[word1[i]]+=1
         else:
            letter[word1[i]]= 1
     print (letter)
@@ -34,15 +32,12 @@ def same_letters(word1,word2):
     string_size2 = len(word2)
     for i in range(string_size2):
         if word2[i] in letter2:
-            count2+=1
-            letter2[word2[i]]=count2
+            letter2[word2[i]]+=1
         else:
             letter2[word2[i]]=1
     print (letter2)
     if letter == letter2:
-        return "True"
+        return True
     else:
-        return "False"
+        return False
 print (same_letters(word1, word2))
-
-#equal_strings (word1,word2)
