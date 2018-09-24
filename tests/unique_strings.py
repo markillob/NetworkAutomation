459be42680be468daf149c45beb13d8966f409be
Marco Barrera <mbarrera@thousandeyes.com>
2018-09-24 10:03:55 -0700

    use items from apython dictionary

--- tests/unique_strings.py
+++ tests/unique_strings.py
@@ -1,5 +1,5 @@
 #coding=utf-8
-string_list=["white","black", "white","marco","orange","red","white"]
+string_list=["brown","white","white","black", "brown","white","marco","orange","red","white"]
 def equal_strings(strings_list):
     string_dictionary={}
     string_list2=string_list
@@ -8,8 +8,10 @@ def equal_strings(strings_list):
        string_dictionary[string_list[i]]="1"
     for i in range(len(string_dictionary)):
         string_list3.append(string_list2[i+1])
-    print(string_list3)
-    return string_dictionary
+    string_list4=[]
+    for i,a in string_dictionary.items():
+        string_list4.append(i)    
+    return string_list4
 
 print (equal_strings(string_list))
 
