63584e7f1d7d2556299e16514e1d6bed3ee03a2a
Marco Barrera <mbarrera@thousandeyes.com>
2018-09-25 08:45:41 -0700

    update unique strings program

--- tests/unique_strings.py
+++ tests/unique_strings.py
@@ -2,7 +2,6 @@
 string_list=["yellow", "orange","brown","white","white","black", "brown","white","marco","orange","red","white"]
 def equal_strings(strings_list):
     string_dictionary={}
-    string_list2=string_list
     for i in range(len(string_list)):
        string_dictionary[string_list[i]]="1"
     string_list4=[]
