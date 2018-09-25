07b45c876350c9a482827ca6367ce0d6753d71aa
Marco Barrera <mbarrera@thousandeyes.com>
2018-09-25 08:44:58 -0700

    Update unique strings program

--- tests/unique_strings.py
+++ tests/unique_strings.py
@@ -1,17 +1,13 @@
 #coding=utf-8
-string_list=["brown","white","white","black", "brown","white","marco","orange","red","white"]
+string_list=["yellow", "orange","brown","white","white","black", "brown","white","marco","orange","red","white"]
 def equal_strings(strings_list):
     string_dictionary={}
     string_list2=string_list
-    string_list3=[]
     for i in range(len(string_list)):
        string_dictionary[string_list[i]]="1"
-    for i in range(len(string_dictionary)):
-        string_list3.append(string_list2[i+1])
     string_list4=[]
     for i,a in string_dictionary.items():
         string_list4.append(i)    
     return string_list4
-
 print (equal_strings(string_list))
 
