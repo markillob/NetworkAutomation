2b6ffcf81f4fdb822f4af768fc4fb79e92e98b06
markillob <cummy9@hotmail.com>
2018-10-09 22:09:15 -0700

    unique strings log(0)

--- go/src/log_string.go
+++ go/src/log_string.go
@@ -4,21 +4,16 @@ import "os"
 import "fmt"
 
 func main() {
-	var stringTest = "markilob"
-	var contador int
-	contador = 1
-	for i := 0; i <= len(stringTest); i++ {
-		for a := i; a <= len(stringTest); a++ {
-			if stringTest[a] == stringTest[i] {
-				contador += 1
-				if contador > 1 {
-					fmt.Println(stringTest[a])
-					os.Exit(1)
-				}
-			} else {
-				contador == 0
+	var stringTest string
+	stringTest="markilo"
+	for i := 0; i < len(stringTest); i++ {
+		for a := (i+1); a < len(stringTest); a++ {
+			if stringTest[i] == stringTest[a] {
+				fmt.Printf("String has duplicate values %c \n",stringTest[i])
+				os.Exit(1)
 			}
 		}
 	}
-	fmt.Println("String has all unique values")
+	fmt.Println("String has no duplicate values ",stringTest)
 }
+	
