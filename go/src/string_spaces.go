d95bd244139376e20221f37ce7f3deee73513268
markillob <cummy9@hotmail.com>
2018-10-11 23:20:48 -0700

    replace strings
    Signed-off-by: markillob <cummy9@hotmail.com>

--- go/src/string_spaces.go
+++ go/src/string_spaces.go
@@ -1,37 +1,85 @@
 package main
 
 import "fmt"
+//import "reflect"
 
 //space is 32
 func main() {
-	var mainString string
-	mainString = "Ho la  "
-	totalLength := len(mainString)
+	//var mainString[7] string
+	mainString := make ([]string,15)
+	mainString[0]="a"
+	mainString[1]="r"
+	mainString[2]="r"
+	mainString[3]="e"
+	mainString[4]=" "
+	mainString[5]="o"
+	mainString[6]="r"
+	mainString[7]="o"
+	mainString[8]=" "
+	mainString[9]="s"
+	mainString[10]="i"
+	mainString[11]=" "
+	mainString[12]=" "
+	mainString[13]=" "
+	mainString[14]=" "
+	fmt.Println(mainString)
+	totalLength :=len(mainString)
 	numberSpaces := 0
 	//Get number of Spaces
 	for i := 0; i < totalLength; i++ {
-		if mainString[i] == 32 {
+		if mainString[i] == " " {
 			numberSpaces++
 		}
-		//fmt.Println(mainString[i])
-		//fmt.Printf("%c", mainString[i])
+			//fmt.Println(mainString[i])
+			//fmt.Printf("%c", mainString[i])
 	}
 	numberSpaces = numberSpaces / 3
+	fmt.Println(numberSpaces)
 	endOfArray := len(mainString) - (numberSpaces * 2)
+	fmt.Println(endOfArray)
+	// move array
+	count:=0
+	endOfArray2:=endOfArray
+	for whiteSpace := 0; whiteSpace < endOfArray; whiteSpace++ {
+		if mainString[whiteSpace] == " " {
+			for z:= len(mainString);z > whiteSpace; z-- {
+				mainString[z] = mainString[endOfArray2]
+				endOfArray2--
+				count++
+			}
+		mainString[whiteSpace] = "%"
+		mainString[whiteSpace+1]="2"
+		mainString[whiteSpace+2]="0"
+		}
+		endOfArray2+=count
+	}
+	//fmt.Println(mainString)
+}
+
+/*	
+	
+	
 	fmt.Println(numberSpaces)
 	fmt.Println(endOfArray)
 	// move array
 	for whiteSpace := 0; whiteSpace < endOfArray; whiteSpace++ {
 		if mainString[whiteSpace] == 32 {
-			var temp1 byte
-			var temp2 byte
-			temp1 = mainString[whiteSpace+1]
-			temp2 = mainString[whiteSpace+2]
-			fmt.Println(temp1)
-			fmt.Println(temp2)
-			mainString[endOfArray] = temp1
-			mainString[endOfArray] = temp1
+			fmt.Println(mainString[whiteSpace])
+			mainString[whiteSpace] = 30
+			fmt.Println(mainString[whiteSpace])
+			//var temp1 byte
+			//var temp2 byte
+			//temp1 = mainString[whiteSpace+1]
+			//temp2 = mainString[whiteSpace+2]
+			//
+			//temp3:= string(mainString[:whiteSpace+2])
+			//fmt.Println(reflect.TypeOf(temp3))
+			//fmt.Println(temp1)
+			//fmt.Println("\n",temp2)
+			//fmt.Println(temp3)
+			//mainString[endOfArray] = temp1
+			//mainString[endOfArray] = temp1
 		}
 	}
-	fmt.Println(mainString)
 }
+*/
